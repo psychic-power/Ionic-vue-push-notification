f0b9f2c4d7845dab11fed59a5a2abc6205f13856
PsychicPower <psycpv@gmail.com>
2023-03-28 10:10:05 +0800

    Project config - appID same with firebase appID

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
+  appId: 'ionic.push.notification',
   appName: 'ionic-push-notification',
   webDir: 'dist',
   bundledWebRuntime: false
